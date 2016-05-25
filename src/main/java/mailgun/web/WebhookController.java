bc5232c0b8e06d19ea65b100c97fa2e181e08fd6
Iaroslav Ciupin <Iaroslav.Ciupin@endava.com>
2016-05-25 21:05:27 +0300

    Webhook

--- src/main/java/mailgun/web/WebhookController.java
+++ src/main/java/mailgun/web/WebhookController.java
@@ -1,5 +1,8 @@
 package mailgun.web;
 
+import mailgun.Main;
+import org.apache.log4j.Logger;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebhookController {
 
+    static Logger logger = Logger.getLogger(Main.class);
+
     @RequestMapping("/status")
     public String status() {
         return "Status: OK";
     }
 
-    @RequestMapping(value = "/webhook", method = RequestMethod.POST)
+    @RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Boolean webhook() {
+        logger.info("Received webhook");
         return true;
     }
 }
