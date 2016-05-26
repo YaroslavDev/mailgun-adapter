87f4e03cb77b565d3d6d18d6aa2496dc70f0d6bf
Iaroslav Ciupin <Iaroslav.Ciupin@endava.com>
2016-05-26 08:12:41 +0300

    finak

--- src/main/java/mailgun/web/WebhookController.java
+++ src/main/java/mailgun/web/WebhookController.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebhookController {
